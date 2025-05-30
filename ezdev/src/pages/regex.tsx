1ed2ab02398f554a50266284e7844c36fef77ce3
Vilius <viliusviskantas@gmail.com>
2025-05-30 15:56:28 +0300

    Added React Router, some basic styling

---
+++ ezdev/src/pages/regex.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../layout/layout';
+
+const Regex: React.FC = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Regex Page</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Regex;
